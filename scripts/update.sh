76bbc721b2c133f796f7105987e751e3dc941fed
Phil Eaton <phil@eatonphil.com>
2022-12-29 19:30:54 -0500

    Update update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,5 +7,5 @@ git reset --hard
 git pull
 sudo rm -rf /usr/share/caddy/*
 sudo mv docs/* /usr/share/caddy/
-sudo chown -R caddy:caddy
+sudo chown -R caddy:caddy /usr/share/caddy/
 sudo restorecon -r /usr/share/caddy/
